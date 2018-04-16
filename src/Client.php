670a0ee8edd96a3c16a06b6bc8182a3414284412
Marin Crnković <marin.crnkovic@gmail.com>
2018-04-16 19:02:38 +0200

    correct default

--- src/Client.php
+++ src/Client.php
@@ -29,7 +29,7 @@ class Client extends Guzzle
         if (!isset($config[$baseUriKey])) {
             $config[$baseUriKey] = static::BASE_URL;
         }
-        $config['defaults']['auth'] = [$username, null];
+        $config['auth'] = [$username, null];
 
         parent::__construct($config);
     }
